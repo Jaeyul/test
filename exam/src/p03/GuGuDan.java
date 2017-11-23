6cc3146ab978fd4b652801e637653be2573ca344
Administrator <Administrator@DJA-PC>
2017-11-23 18:33:21 +0900

    함수 생성자 -> 생성, 소멸 구간 and 선언할 때만 사용 가능

---
+++ exam/src/p03/GuGuDan.java
@@ -0,0 +1,28 @@
+package p03;
+
+public class GuGuDan {
+	
+	void printLoop(ObjectExam oe) { 
+		for(int i=1; i<10; i++) {
+			for(int j=1; j<10; j++) {
+//				System.out.print(j+"X"+i+"="+(i*j) + ",");
+//				System.out.print(j+"X"+i+"=");
+				System.out.print("["+i+","+j+"]");
+//				if(j==9) {
+//					System.out.print(j+"X"+i+"="+(i*j));
+//				}
+			}System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		
+//		for(int i=1; i<10; i++) {
+//			System.out.print("1X" + i + "=" + i+",");
+//		}for(int i=1; i<10; i++) {
+//			System.out.print("2X" + i + "=" + 2*i+",");
+//		}
+		
+	}				
+}
+
+
---
+++ exam/src/p03/Test.java
@@ -0,0 +1,53 @@
+package p03;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	String str;
+	
+	Test(String str) {
+		this.str = str;
+	}
+	void print() {
+		System.out.println(str);
+	}
+//	static void printInt(int a) {
+//		System.out.println(a);
+//	}
+	
+	String getCoffee() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("돈을 넣으세요");
+		int pay = s.nextInt();
+		if(pay>1000) {
+			return "커피 사왔습니다.";
+		}return "돈 부족 함";
+	}
+	
+	public static void main(String[] args) {
+//		ObjectExam oe = new ObjectExam();
+//		oe.inputNums();
+//		oe.printLoop();
+//		System.out.println(oe.num1 + oe.num2);
+		
+		Test t = new Test("가나다ABC123");  //위에서 Test(String str)로 선언을 했기에  String에 맞게 문자열 필요하다.
+//		t.print();
+		String result = t.getCoffee();
+		System.out.println(result);
+		
+//		String str = "나 나여깄어요~~나";
+		
+//		str = str.replace("나","최재열");
+//		t.print(str.replace("나", "최재열"));
+//		t.print(str);
+		
+//		System.out.println(str.indexOf("나"));
+//		System.out.println(str.lastIndexOf("나"));
+//		System.out.println(str.substring(1));
+//		str = str.substring(3);
+//		System.out.println(str.indexOf("나"));
+		
+	}
+
+}
