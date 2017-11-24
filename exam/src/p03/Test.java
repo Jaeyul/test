7154eb47400f8f6d3f40ff80bec4db4798bd781d
jaeyul <gauk87@naver.com>
2017-11-24 10:47:20 +0900

    ??

--- exam/src/p03/Test.java
+++ exam/src/p03/Test.java
@@ -16,25 +16,33 @@ public class Test {
 //		System.out.println(a);
 //	}
 	
-	String getCoffee() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("돈을 넣으세요");
-		int pay = s.nextInt();
-		if(pay>1000) {
-			return "커피 사왔습니다.";
-		}return "돈 부족 함";
-	}
+//	String getCoffee() {
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("돈을 넣으세요");
+//		int pay = s.nextInt();
+//		if(pay>1000) {
+//			return "커피 사왔습니다.";
+//		}return "돈 부족 함";
+//	}
 	
 	public static void main(String[] args) {
+		ObjectExam oe = new ObjectExam();
+		oe.inputNums();		
+		
+		GuGuDan ggd = new GuGuDan();
+		ggd.printLoop(oe);
+		
+		
 //		ObjectExam oe = new ObjectExam();
 //		oe.inputNums();
 //		oe.printLoop();
 //		System.out.println(oe.num1 + oe.num2);
 		
-		Test t = new Test("가나다ABC123");  //위에서 Test(String str)로 선언을 했기에  String에 맞게 문자열 필요하다.
+//		Test t = new Test("가나다ABC123");  //위에서 Test(String str)로 선언을 했기에  String에 맞게 문자열 필요하다, 여기서 변수 t는 위의 함수들을 호출가능
 //		t.print();
-		String result = t.getCoffee();
-		System.out.println(result);
+		
+//		String result = t.getCoffee();
+//		System.out.println(result);
 		
 //		String str = "나 나여깄어요~~나";
 		
