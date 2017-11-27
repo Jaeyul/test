12bcdc551b367754d42d3ffd84208c153f285b3d
jaeyul <gauk87@naver.com>
2017-11-27 18:39:02 +0900

    랩퍼클래스, int형 배열 변수, String 배열 변수

---
+++ exam/src/p04/Fibonacci.java
@@ -0,0 +1,18 @@
+package p04;
+
+public class Fibonacci {
+	
+	void fibo() {	
+		
+		int sum;
+		int[] num = new int[1];			
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
