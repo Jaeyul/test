50f0816a89f27f2d09ddef20eff784e45bde11b2
jaeyul <gauk87@naver.com>
2017-11-29 18:36:55 +0900

    신세계 생각의 폭을 넓혀라!

--- exam/src/p01/Exam01.java
+++ exam/src/p01/Exam01.java
@@ -4,7 +4,8 @@ public class Exam01 {
 		
 	public static void main(String[] args) {
 			int a = 3;
-			int b = 4;			
+			int b = 4;		
+			int c = b;
 			System.out.println(a*b);
 			System.out.println(a+b);
 			System.out.println(a-b);
